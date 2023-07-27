331c502ba7e8945e9c0cbea8414e8ae3c6146d9c
Vitor Mancio Silva <vitordeveth@gmail.com>
2023-07-27 10:33:41 -0300

    ajust truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -2,13 +2,13 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*"
     }
   },
   compilers: {
     solc: {
-      version: "0.5.1"
+      version: "0.8.0"
     }
   }
 };
